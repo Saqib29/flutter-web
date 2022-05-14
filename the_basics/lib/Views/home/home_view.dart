6ee0db38ec71fd548efa88e7e04621b20317fac8
saqib <saqib29111@gmail.com>
2022-05-14 18:33:43 +0600

    add dourse_details widget

--- the_basics/lib/Views/home/home_view.dart
+++ the_basics/lib/Views/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:the_basics/widgets/centered_View/centered_view.dart';
+import 'package:the_basics/widgets/course_details/course_details.dart';
 
 import 'package:the_basics/widgets/navigation_bar/navigation_bar.dart';
 
@@ -12,8 +13,15 @@ class HomeView extends StatelessWidget {
       backgroundColor: Colors.white,
       body: CenteredView(
         child: Column(
-          children: const [
-            NavigationBarCustom()
+          children: [
+            NavigationBarCustom(),
+            Expanded(
+              child: Row(
+                children: const [
+                  CourseDetails()
+                ],
+              ),
+            )
           ],
         ),
       ),
